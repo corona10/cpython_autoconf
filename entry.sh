8cbd91486fbaccccc6579ae2d48dd105602a7e70
Erlend E. Aasland <erlend@python.org>
2024-03-03 01:00:09 +0100

    Entryscript: don't check sentinel if we're running arbitrary commands

--- entry.sh
+++ entry.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 set -e
 
-SENTINEL=/src/pyconfig.h.in
-
-if [ ! -e ${SENTINEL} ]; then
-    echo "ERROR: ${SENTINEL} not found "
-    echo "Did you forget to mount Python work directory with '-v.:/src'?"
-    echo
-    echo "  docker run -v\$PWD:/src ghcr.io/corona10/cpython_autoconf"
-    echo "  podman run -v\$PWD:/src:Z ghcr.io/corona10/cpython_autoconf"
-    exit 2
-fi
-
 BIN=/usr/local/bin
 AUTOCONF=$BIN/autoconf
 AUTORECONF=$BIN/autoreconf
 
 if [ "$#" = "0" ]; then
+    local SENTINEL=/src/pyconfig.h.in
+
+    if [ ! -e ${SENTINEL} ]; then
+        echo "ERROR: ${SENTINEL} not found "
+        echo "Did you forget to mount Python work directory with '-v.:/src'?"
+        echo
+        echo "  docker run -v\$PWD:/src ghcr.io/corona10/cpython_autoconf"
+        echo "  podman run -v\$PWD:/src:Z ghcr.io/corona10/cpython_autoconf"
+        exit 2
+    fi
+
     echo "Rebuilding configure script using $($AUTOCONF --version | head -n 1)"
     exec gosu $AUTORECONF -ivf -Werror $@
 fi
