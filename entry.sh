e88aa4574a01e74d690e8288dae014021b47d4cc
Erlend E. Aasland <erlend@python.org>
2024-03-04 15:13:13 +0100

    Updates (#12)
    * CI: Add build matrix
    
    * Entryscript: use explicit paths for invocation
    
    * Dockerfile: add build args for versions, and install automake and autoconf-archive
    
    * Entryscript: don't check sentinel if we're running arbitrary commands
    
    * Adjust CI name
    
    * Remove QEMU from build/test CI

--- entry.sh
+++ entry.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 set -e
 
-SRC=/src
-SENTINEL=${SRC}/pyconfig.h.in
-
-if [ ! -e ${SENTINEL} ]; then
-    echo "ERROR: ${SENTINEL} not found "
-    echo "Did you forget to mount Python work directory with '-v.:/src'?"
-    echo
-    echo "  docker run -v.:/src dongheena/cpython_autoconf"
-    echo "  podman run -v.:/src:Z dongheena/cpython_autoconf"
-    exit 2
-fi
+BIN=/usr/local/bin
+AUTOCONF=$BIN/autoconf
+AUTORECONF=$BIN/autoreconf
 
 if [ "$#" = "0" ]; then
-    echo "Rebuilding configure script using $(autoconf --version | head -n 1)"
-    exec gosu autoreconf -ivf -Werror $@
+    local SENTINEL=/src/pyconfig.h.in
+
+    if [ ! -e ${SENTINEL} ]; then
+        echo "ERROR: ${SENTINEL} not found "
+        echo "Did you forget to mount Python work directory with '-v.:/src'?"
+        echo
+        echo "  docker run -v\$PWD:/src ghcr.io/corona10/cpython_autoconf"
+        echo "  podman run -v\$PWD:/src:Z ghcr.io/corona10/cpython_autoconf"
+        exit 2
+    fi
+
+    echo "Rebuilding configure script using $($AUTOCONF --version | head -n 1)"
+    exec gosu $AUTORECONF -ivf -Werror $@
 fi
 
 exec "$@"
